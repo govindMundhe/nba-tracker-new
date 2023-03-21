2fa74cf431b524777f9fe96ec234ac958a634433
govindMundhe <govindmundhe452@gmail.com>
2023-03-21 19:05:04 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
 import { AppModule } from './app/app.module';
 
 
